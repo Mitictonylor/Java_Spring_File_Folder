e4697ab2799c20d12eea9378ddbcd646b852c052
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-09 18:46:59 +0100

    add Post

--- src/main/java/com/github/Mitictonylor/Files/fileManagement/controller/UserController.java
+++ src/main/java/com/github/Mitictonylor/Files/fileManagement/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,4 +31,13 @@ public class UserController {
     public ResponseEntity getUser(@PathVariable Long id){
         return new ResponseEntity(userRepository.findById(id), HttpStatus.OK);
     }
+
+    //create route, add a new instance of user in the db
+    @GetMapping(value = "/users")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+
 }
