400f7d95f6997330034b57a6bfd4e12369fcb68e
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-09 18:43:27 +0100

    create show route and index route

---
+++ src/main/java/com/github/Mitictonylor/Files/fileManagement/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.github.Mitictonylor.Files.fileManagement.controller;
+
+import com.github.Mitictonylor.Files.fileManagement.modules.User;
+import com.github.Mitictonylor.Files.fileManagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+//index route, all users
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+
+    //show route, show a particular user
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity getUser(@PathVariable Long id){
+        return new ResponseEntity(userRepository.findById(id), HttpStatus.OK);
+    }
+}
